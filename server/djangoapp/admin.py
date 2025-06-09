1ce4469fe1624b5b84d00ea5d2caa3a49afe8e1a
Luis Castro <furaki.aics@gmail.com>
2025-06-09 15:28:08 -0500

    Fix linting errors

--- server/djangoapp/admin.py
+++ server/djangoapp/admin.py
@@ -4,22 +4,26 @@ from .models import CarMake, CarModel
 
 # Register your models here.
 
+
 # CarModelInline class
 class CarModelInline(admin.StackedInline):
     model = CarModel
     extra = 1
 
+
 # CarModelAdmin class
 class CarModelAdmin(admin.ModelAdmin):
-    list_display = ('name', 'car_make', 'type', 'year')
-    list_filter = ('car_make', 'type', 'year')
-    search_fields = ('name', 'car_make__name')
+    list_display = ("name", "car_make", "type", "year")
+    list_filter = ("car_make", "type", "year")
+    search_fields = ("name", "car_make__name")
+
 
 # CarMakeAdmin class with CarModelInline
 class CarMakeAdmin(admin.ModelAdmin):
     inlines = [CarModelInline]
-    list_display = ('name', 'description')
-    search_fields = ('name',)
+    list_display = ("name", "description")
+    search_fields = ("name",)
+
 
 # Register models here
 admin.site.register(CarMake, CarMakeAdmin)
--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -5,26 +5,26 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-backend_url = os.getenv(
-    'backend_url', default="http://localhost:3030")
+backend_url = os.getenv("backend_url", default="http://localhost:3030")
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/")
+    "sentiment_analyzer_url", default="http://localhost:5050/"
+)
 
 # def get_request(endpoint, **kwargs):
 # **kwargs es un diccionario de argumentos que se pasan a la funcion
 # por ejemplo **kwargs = {"key1": "value1", "key2": "value2"}
 
+
 def get_request(endpoint, **kwargs):
     # endpoint es la url de la peticion
     # ** hace que se pasen los argumentos como un diccionario
     params = ""
-    if(kwargs):
-        for key,value in kwargs.items():
-            params=params+key+"="+value+"&"
+    if kwargs:
+        for key, value in kwargs.items():
+            params = params + key + "=" + value + "&"
         # se convierte en key1=value1&key2=value2 en la url de la peticion
-      
-    request_url = backend_url+endpoint+"?"+params
+
+    request_url = backend_url + endpoint + "?" + params
     # backend_url es la url de la api de mongoDB
     # endpoint es la funcion que se va a llamar en la api de mongoDB
 
@@ -33,13 +33,13 @@ def get_request(endpoint, **kwargs):
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
         return response.json()
-    except:
+    except Exception:
         # If any error occurs
         print("Network exception occurred")
 
 
 def analyze_review_sentiments(text):
-    request_url = sentiment_analyzer_url+"analyze/"+text
+    request_url = sentiment_analyzer_url + "analyze/" + text
     try:
         # Call get method of requests library with URL and parameters
         response = requests.get(request_url)
@@ -48,15 +48,17 @@ def analyze_review_sentiments(text):
         print(f"Unexpected {err=}, {type(err)=}")
         print("Network exception occurred")
 
+
 # Add code for retrieving sentiments
+
 
 # def post_review(data_dict):
 # Add code for posting review
 def post_review(data_dict):
-    request_url = backend_url+"/insert_review"
+    request_url = backend_url + "/insert_review"
     try:
-        response = requests.post(request_url,json=data_dict)
+        response = requests.post(request_url, json=data_dict)
         print(response.json())
         return response.json()
-    except:
+    except Exception:
         print("Network exception occurred")
